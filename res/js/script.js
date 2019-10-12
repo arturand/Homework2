dcc16bfc963bc46d50dd4cf9937899308f9701b0
arturand <44437751+arturand@users.noreply.github.com>
2019-10-12 12:43:19 +0300

    correct files

---
+++ res/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    var user = new User(
+        "Siim",
+        "Anderson",
+        "12.04.1997",
+        "Computer Science",
+        5.0,);
+
+    var users;
+    users = new User("Siim", "Anderson", "12.04.1997", "Computer Science", 5.0);
+
+    var course = new Course()
+    var courses;
+    courses = [
+        new Course("Operation systems", "First Semester", 4.0),
+        new Course("Software Engineering", "First Semester", 3.0),
+        new Course("Algorithms and Data Structures", "First Semester", 5.0),
+        new Course("Introduction to Data Science", "First semester", 3.0)
+    ];
+
+
+});
